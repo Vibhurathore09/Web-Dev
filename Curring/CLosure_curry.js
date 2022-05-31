fad38b6ae42b8e9c81d7f5cca20c88ddb181d0a7
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2022-05-31 06:08:37 -0700

    Delete CLosure_curry.js

--- Curring/CLosure_curry.js
+++
@@ -1,8 +0,0 @@
-function add(x){
-    return function(y){
-        console.log(x+y)
-    }
-}
-
-let addWith2 = add(2) // 2 is passed in add function and returned a function 
-addWith2(3) // this 3 is passed in returned function 
