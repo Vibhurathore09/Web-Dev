efa11594347460488ecdf2e6fb31d1ad84a87072
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2022-06-13 16:33:02 +0530

    Add files via upload

---
+++ JS/concepts/FlattenObj.js
@@ -0,0 +1,12 @@
+function flattenObj(onj , parent , res= {}){
+    for(let key in obj){
+        let propName = parent ? parent + '_' + key : key;
+        if(typeof obj[key] == 'object'){
+            flattenObj (obj[key] , propName , res);
+        }else{
+            res[propName] = obj[key];
+        }
+        
+    }
+    return res;
+}
