596170b4376ec75de406cde23c42bc57e1d77707
Vibhu Rathore <79627056+Vibhurathore09@users.noreply.github.com>
2022-06-13 14:11:13 +0530

    Add files via upload

---
+++ JS/concepts/Undefined.js
@@ -0,0 +1,24 @@
+// console.log(a)
+// var a = 2
+
+
+// undeifined -> variable exits but value in not given
+
+// not defined -> variable does not exits
+
+// ----- unless some value is not initalized to some variable or function then it is undefined.
+
+// NUll is a value which shows empty value
+
+
+function test(){
+    // if it returns nothing then it is undefined
+    return null
+}
+// console.log(test())
+
+
+let b = global.v
+console.log(b)
+
+// null -> 
